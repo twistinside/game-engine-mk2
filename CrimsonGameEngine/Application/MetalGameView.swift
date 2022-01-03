27b5feb8cd64ab4562c2c2d4a96f9862fb95c2af
twistinside <twistinside@users.noreply.github.com>
2022-01-02 22:03:57 -0800

    Refactor rendering out of game view

--- CrimsonGameEngine/Application/MetalGameView.swift
+++ CrimsonGameEngine/Application/MetalGameView.swift
@@ -1,56 +1,14 @@
 import MetalKit
 
 class MetalGameView: MTKView {
-    
-    var commandQueue: MTLCommandQueue?
-    var renderPipelineState: MTLRenderPipelineState?
-    
-    let vertices: [float3] = [
-        float3( 0, 1, 0), // Top middle
-        float3(-1,-1, 0), // Bottom left
-        float3( 1,-1, 0)  // Bottom right
-    ]
+    var renderer: Renderer?
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
         self.device = MTLCreateSystemDefaultDevice()
         self.clearColor = MTLClearColor(red: 0.73, green: 0.23, blue: 0.35, alpha: 1.0)
         self.colorPixelFormat = .bgra8Unorm
-        self.commandQueue = device?.makeCommandQueue()
-        
-        let library = device?.makeDefaultLibrary()
-        let vertexFunction = library?.makeFunction(name: "basicVertexShader")
-        let fragmentFunction = library?.makeFunction(name: "basicFragmentShader")
-        
-        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
-        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        renderPipelineDescriptor.vertexFunction = vertexFunction
-        renderPipelineDescriptor.fragmentFunction = fragmentFunction
-        
-        do {
-            renderPipelineState = try device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
-        } catch let error {
-            fatalError(error.localizedDescription)
-        }
-    }
-    
-    override func draw(_ dirtyRect: NSRect) {
-        guard let drawable = self.currentDrawable,
-              let renderPassDescriptor = self.currentRenderPassDescriptor,
-              let commandBuffer = commandQueue!.makeCommandBuffer(),
-              let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        else {
-            return
-        }
-        
-        let vertexBuffer = device?.makeBuffer(bytes: vertices, length: MemoryLayout<float3>.stride * vertices.count, options: [])
-        
-        renderCommandEncoder.setRenderPipelineState(renderPipelineState!)
-        renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderCommandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
-        renderCommandEncoder.endEncoding()
-
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
+        self.renderer = Renderer(self)
+        self.delegate = renderer
     }
 }
---
+++ CrimsonGameEngine/Renderer/Renderer.swift
@@ -0,0 +1,63 @@
+import MetalKit
+
+class Renderer: NSObject {
+    var device: MTLDevice?
+    var commandQueue: MTLCommandQueue?
+    var renderPipelineState: MTLRenderPipelineState?
+    
+    let vertices: [float3] = [
+        float3( 0, 1, 0), // Top middle
+        float3(-1,-1, 0), // Bottom left
+        float3( 1,-1, 0)  // Bottom right
+    ]
+    
+    init(_ view: MTKView) {
+        self.device = view.device
+        self.commandQueue = device?.makeCommandQueue()
+        super.init()
+    }
+}
+
+extension Renderer: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        // TODO: Respond to window resize
+    }
+    
+    func draw(in view: MTKView) {
+        guard let drawable = view.currentDrawable,
+              let renderPassDescriptor = view.currentRenderPassDescriptor,
+              let commandBuffer = commandQueue!.makeCommandBuffer(),
+              let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        else {
+            return
+        }
+        
+        let library = device?.makeDefaultLibrary()
+        let vertexFunction = library?.makeFunction(name: "basicVertexShader")
+        let fragmentFunction = library?.makeFunction(name: "basicFragmentShader")
+        
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineDescriptor.vertexFunction = vertexFunction
+        renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        
+        do {
+            renderPipelineState = try device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        } catch let error {
+            fatalError(error.localizedDescription)
+        }
+        
+        let vertexBuffer = device?.makeBuffer(bytes: vertices, length: MemoryLayout<float3>.stride * vertices.count, options: [])
+        
+        renderCommandEncoder.setRenderPipelineState(renderPipelineState!)
+        renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderCommandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
+        renderCommandEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+    
+    
+}
+
