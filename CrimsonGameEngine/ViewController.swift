d840e85d15fbcf78dc095247a6f02a5e36f5ca27
twistinside <twistinside@users.noreply.github.com>
2022-01-02 17:10:28 -0800

    Initial Commit

---
+++ CrimsonGameEngine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CrimsonGameEngine
+//
+//  Created by Karl Groff on 1/2/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
